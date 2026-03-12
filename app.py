f2e340a4e038dd2fcc6b11018f1da6343c76cda2
Mixolydien <tristan.isambart@gmail.com>
2026-03-12 16:08:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def predict_by_skills():
     return jsonify({"suggestions": suggestions})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
